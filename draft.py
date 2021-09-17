d6808390905ffcb09673a49298f8797419c18c9a
GoneSahlin <zach@sahlins.net>
2021-09-17 10:46:19 -0700

    initial commit

---
+++ draft.py
@@ -0,0 +1,9 @@
+class Draft:
+    # constructor
+    def __init__(self):
+        print("init")
+        self.y = "init ran"
+
+    # print("init2")
+    def f(self):
+        print(self.y)
---
+++ main.py
@@ -0,0 +1,8 @@
+import draft
+
+def main():
+    x = draft.Draft()
+    x.f()
+    print("end")
+
+main()
