addd6c84c4e7dd6fa459b313202e5845a235cdc0
GoneSahlin <zach@sahlins.net>
2021-09-19 16:24:40 -0700

    added empty functions to draft.py

--- draft.py
+++ draft.py
@@ -4,6 +4,42 @@ class Draft:
         print("init")
         self.y = "init ran"
 
-    # print("init2")
-    def f(self):
-        print(self.y)
+    # read in the player data from csv
+    def read_players(self, filename):
+        pass
+
+    # sorts the players into positions
+    def sort_players(self):
+        pass
+
+    # calculates draft order based on number of teams with a snake draft
+    def calculate_draft_order(self):
+        pass
+
+    # initializes the teams
+    def create_teams():
+        pass
+
+    # starts the draft
+    def start(self):
+        pass
+
+    # takes the next turn of the draft
+    def next_turn(self):
+        pass
+
+    # gets the next teams turn
+    def get_next_team(self):
+        pass
+
+    # ends the draft and projects rankings
+    def end(self):
+        pass
+
+    # gets the player list
+    def get_players(self):
+        pass
+
+    # gets the players with position pos
+    def get_players(self, pos):
+        pass
