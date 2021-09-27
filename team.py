466981c5a61229ec9995d3c6654546d8ae7de19e
GoneSahlin <zach@sahlins.net>
2021-09-27 00:05:50 -0700

    added empty functions to team #1

--- team.py
+++ team.py
@@ -4,16 +4,37 @@ A fantasy football team
 Author: Zach Sahlin
 """
 
+import pandas as pd
+
+
 class Team:
     """A fantasy football team"""
 
     def __init__(self):
         """Constructor"""
-        pass
 
+        my_players_df = pd.DataFrame()
 
     def add_player(self):
         """Adds a player to the team
 
         :return:
         """
+
+        pass
+
+    def get_unfilled_positions(self):
+        """Gets the positions that haven't been filled yet
+
+        :returns unfilled_positions: the positions on the team that are empty
+        """
+
+        pass
+
+    def make_selection(self):
+        """Makes the next pick
+
+        :return:
+        """
+
+        pass
