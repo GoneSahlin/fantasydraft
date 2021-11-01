9810a23b13eee7bd4236b0000dec3a2890cd1630
GoneSahlin <zach@sahlins.net>
2021-11-01 10:46:00 -0700

    added create_children to TreeNode #2

--- tree_team.py
+++ tree_team.py
@@ -14,7 +14,7 @@ class TreeTeam(Team):
         """Makes a selection using a tree algorithm.
         """
         current_state = self.draft.get_players()
-        
+
         pass
 
     def create_tree(self):
@@ -26,7 +26,30 @@ class TreeTeam(Team):
         """A node in the tree
         """
 
-        def __init__(self, state):
+        def __init__(self, players, empty_pos, depth, parent):
             """
             """
-            self.state = state
+            self.players = players
+            self.empty_pos = empty_pos
+            self.depth = depth
+            self.parent = parent
+            self.children = []
+
+        def create_children(self, draft):
+            """Create the children of this node
+            """
+            unique_pos = [pos for n, pos in enumerate(self.empty_pos) if pos not in self.empty_pos[:n]]
+            for pos in unique_pos:
+                # find best player in position
+                player_index = self.players.groupby('Position').get_group(pos).head(1).index
+
+                print(player_index)
+
+
+                # create a child
+
+                child = self.__class__()
+
+                self.children.append(child)
+
+
