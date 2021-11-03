875890b0882cc92c347c4626476c7527b75a3d65
GoneSahlin <zach@sahlins.net>
2021-11-03 10:26:32 -0700

    added empty functions #3

---
+++ mean_team.py
@@ -0,0 +1,21 @@
+from team import Team
+
+
+class MeanTeam(Team):
+    """Chooses players based on how it will affect the mean of potential future picks
+    """
+
+    def __init__(self):
+        """Constructor
+        """
+        pass
+
+    def make_selection(self):
+        """Makes the next pick
+        """
+        pass
+
+    def make_matrix(self, draft):
+        """Makes a matrix of projected picks
+        """
+        pass
